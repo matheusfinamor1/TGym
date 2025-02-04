bcaf0fe2bcc9024cc1097be10e9729fcf206de51
matheusfinamor1 <matheusfinamor1@gmail.com>
2025-02-04 17:45:47 -0300

    implementation inputEditText email and password of the screen SignIn

--- app/src/main/java/com/matheusfinamor/tgym/MainActivity.kt
+++ app/src/main/java/com/matheusfinamor/tgym/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.matheusfinamor.tgym
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.matheusfinamor.tgym.databinding.ActivityMainBinding
 
@@ -12,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
     }
 }
